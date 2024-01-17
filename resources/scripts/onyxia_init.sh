155f6b39ecb3d93f9cef90d5a52d77459f21f7a9
Anders Lunde <8294494+Andilun@users.noreply.github.com>
2024-01-17 12:56:11 +0100

    Revert "Feature: Mount GCS Bucket" (#2)

--- resources/scripts/onyxia_init.sh
+++
@@ -1,7 +0,0 @@
-nohup python3 -m gcsfuse_token_provider &> "$HOME/.gcsfuse_token_provider.out" &
-if [ -n "$GCS_MOUNT_BUCKET" ]; then
-  mkdir "$HOME/mnt/$GCS_MOUNT_BUCKET"
-  gcsfuse --token-url="http://127.0.0.1:5555/credentials" "$GCS_MOUNT_BUCKET" "$HOME/mnt/$GCS_MOUNT_BUCKET"
-fi
-echo "execution of $*"
-exec "$@" "$INIT_PATH"
